78609f2a1586852ece9d6543469a2b8d8ae00248
Ilshat Gelmutdinov <gelmutdinov1988@mail.ru>
2022-09-01 14:05:22 +0300

    Init 1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("TestProjectForGitAndGithub");
+        System.out.println("TestProjectForGitAndGithub");
     }
 }
